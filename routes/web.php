249553b7e507640ac9613c829576f605c4b565a8
tobimaru1227 <tobimaru1227@gmail.com>
2023-06-20 20:43:38 +0900

    Tweetコントローラー作成とindexアクション定義

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ use App\Http\Controllers\TweetController;
 
 Route::get('/', [TweetController::class, 'index'])->name('tweet.index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
