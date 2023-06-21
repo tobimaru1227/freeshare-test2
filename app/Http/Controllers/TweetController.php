a755885b939a3d94a22c8bf6d25984f84cee3fdd
tobimaru1227 <tobimaru1227@gmail.com>
2023-06-21 19:37:46 +0900

    createアクションの定義

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -7,9 +7,22 @@ use App\Models\Tweet;
 
 class TweetController extends Controller
 {
+    /**
+     * トップページを表示
+     * @return view
+     */
     public function index()
     {
         $tweets = Tweet::all();
         return view('tweets.index', ['tweets' => $tweets]);
+    }
+    
+    /**
+     * 投稿ページを表示
+     * @return view
+     */
+    public function create()
+    {
+        return view('tweets.create');
     }
 }
